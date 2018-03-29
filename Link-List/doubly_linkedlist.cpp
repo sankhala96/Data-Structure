2e9eb43abdaad8a3aa48b0354f40920a7632376b
sankhala <sankhalayash96@gmail.com>
2018-03-29 21:08:51 +0530

    insert at end dll

--- Link-List/doubly_linkedlist.cpp
+++ Link-List/doubly_linkedlist.cpp
@@ -21,6 +21,22 @@ void insertAtFront(struct Node** head, int x){
 	
 }
 
+void insertAtEnd(struct Node** head, int x){
+	struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+	
+	newNode -> data = x;
+	newNode -> next = NULL;
+	
+	struct Node *last = *head;
+	while(last->next != NULL){
+		last = last -> next;
+	}
+	
+	last -> next = newNode;
+	
+	newNode -> prev = last;
+}
+
 void print(struct Node* head){
 	while(head != NULL){
 		cout<<(head->data)<<" ";
@@ -35,7 +51,7 @@ int main(){
 	insertAtFront(&head, 1);
 	insertAtFront(&head, 2);
 	insertAtFront(&head, 3);
-	
+	insertAtEnd(&head, 4);
 	print(head);
 	
 }
